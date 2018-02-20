4e43fc89d9fecfef265193ff3ae88488d86fcfb0
Jack Hayford <Jack.hayford@alphasights.com>
2018-02-20 10:01:57 -0500

    Refactor Score Breakdown Query, Set Controller Appropriately

--- app/score-breakdown/route.js
+++ app/score-breakdown/route.js
@@ -1,6 +1,5 @@
 import Route from '@ember/routing/route';
-import { isEmpty } from '@ember/utils';
-import { get, set } from '@ember/object';
+import { set } from '@ember/object';
 import RouteQueryManager from "ember-apollo-client/mixins/route-query-manager";
 import scoreBreakdownQuery from "lion/gql/queries/score-breakdowns";
 
@@ -16,12 +15,9 @@ export default Route.extend(RouteQueryManager, {
   },
 
   setupController(controller, model) {
-    set(controller, 'scores', model);
+    set(controller, 'scores', model.scores);
 
-    if (isEmpty(get(controller, 'user'))) {
-      const users = model.mapBy('user');
-      const { user_id } = this.paramsFor('score-breakdown');
-      set(controller, 'user', users.findBy('id', user_id));
-    }
+    delete model.scores
+    set(controller, 'user', model);
   },
 });
