c28a0b0602c8353ec518c143afea299e92716017
Jack Hayford <Jack.hayford@alphasights.com>
2018-03-23 10:15:10 -0400

    Added coverage for feed route and pull request component

--- tests/integration/components/pull-request/component-test.js
+++ tests/integration/components/pull-request/component-test.js
@@ -1,3 +1,4 @@
+import { set } from '@ember/object';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
@@ -6,19 +7,26 @@ moduleForComponent('pull-request', 'Integration | Component | pull request', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  assert.expect(4);
+  const user = {
+    avatarUrl: 'http://placeholder.it/350x350',
+    nickname: 'John Smith'
+  };
+  const score = { points: 50 };
+  const pullRequest = {
+    additions: 200,
+    deletions: 300,
+    name: 'Lion',
+    number: 12,
+    title: 'Added PR Feed',
+    score: score,
+    user: user
+  };
+  set(this, 'pullRequest', pullRequest);
+  this.render(hbs`{{pull-request pullRequest=pullRequest}}`);
 
-  this.render(hbs`{{pull-request}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#pull-request}}
-      template block text
-    {{/pull-request}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.summary').text().trim(), 'John Smith merged Added PR Feed.');
+  assert.equal(this.$('.number--points').text().trim(), '50');
+  assert.equal(this.$('.number--additions').text().trim(), '+ 200');
+  assert.equal(this.$('.number--deletions').text().trim(), '- 300');
 });
--- tests/unit/feed/route-test.js
+++ tests/unit/feed/route-test.js
@@ -1,8 +1,7 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:feed', 'Unit | Route | feed', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:apollo', 'service:session']
 });
 
 test('it exists', function(assert) {
